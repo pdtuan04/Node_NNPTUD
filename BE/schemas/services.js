12fb66baac11baa6264bb7b2eb41a15c07b74b04
handzvcl <truonghan4002@gmail.com>
2026-04-05 00:40:41 +0700

    Build Service Management

--- BE/schemas/services.js
+++ BE/schemas/services.js
@@ -4,44 +4,50 @@ const serviceSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Tên dịch vụ là bắt buộc"]
+      required: [true, "Tên dịch vụ là bắt buộc"],
     },
     description: {
       type: String,
-      default: ""
+      default: "",
     },
     price: {
       type: Number,
       default: 0,
-      min: [0, "Giá tiền không được âm"]
+      min: [0, "Giá tiền không được âm"],
     },
     durationInMinutes: {
       type: Number,
-      default: 1,
-      min: [1, "Thời gian thực hiện tối thiểu là 1 phút"]
+      default: 15,
+      min: [15, "Thời gian thực hiện tối thiểu là 15 phút"],
     },
     isActive: {
       type: Boolean,
-      default: true
+      default: true,
     },
     isDeleted: {
       type: Boolean,
-      default: false
+      default: false,
     },
     petTypes: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "petType"
-      }
+        ref: "petType",
+      },
     ],
     imageUrl: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
 );
+
+serviceSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
 
 module.exports = mongoose.model("service", serviceSchema);
